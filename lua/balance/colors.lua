b93eac7352553d2df3de1acff3aa322cb18640a6
Arjun <ccldarjun@icloud.com>
2024-11-25 15:28:56 -0800

    change purewhite

--- lua/balance/colors.lua
+++ lua/balance/colors.lua
@@ -25,7 +25,7 @@ local color = {
   indigo     = '#2e00c9',
   purple     = '#a300f4',
 
-  purewhite  = '#ffffff',
+  purewhite  = '#efefef',
   white      = '#efefef',
   offwhite   = '#dedede',
   lightgrey  = '#d3d3d3',
